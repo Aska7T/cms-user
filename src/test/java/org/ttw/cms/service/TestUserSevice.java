dadf96d99c8b7a8c58fb51deef96ec864bf97651
ttwDW <taotw@hualu.com.cn>
2016-08-09 14:47:38 +0800

    完成userService验证

--- src/test/java/org/ttw/cms/service/TestUserSevice.java
+++ src/test/java/org/ttw/cms/service/TestUserSevice.java
@@ -2,6 +2,10 @@ package org.ttw.cms.service;
 
 import javax.inject.Inject;
 import static org.easymock.EasyMock.*;
+
+import java.util.Arrays;
+import java.util.List;
+
 import org.easymock.EasyMock;
 import org.junit.Assert;
 import org.junit.Test;
@@ -12,6 +16,10 @@ import org.ttw.basic.model.Pager;
 import org.ttw.cms.dao.IGroupDao;
 import org.ttw.cms.dao.IRoleDao;
 import org.ttw.cms.dao.IUserDao;
+import org.ttw.cms.model.CmsException;
+import org.ttw.cms.model.Group;
+import org.ttw.cms.model.Role;
+import org.ttw.cms.model.RoleType;
 import org.ttw.cms.model.User;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -55,6 +63,18 @@ public class TestUserSevice {
 		verify(userDao);
 	}
 	
+	@Test(expected=CmsException.class)
+	public void testUpdateStatusNoUser(){
+		reset(userDao);
+		int uid = 2;
+		expect(userDao.load(uid)).andReturn(null);
+		expectLastCall();
+		replay(userDao);
+		userService.updateStatus(uid);
+		Assert.assertEquals(baseUser.getStatus(),1);
+		verify(userDao);
+	}
+	
 	@Test
 	public void testFindUser(){
 		reset(userDao);
@@ -62,5 +82,146 @@ public class TestUserSevice {
 		replay(userDao);
 		userService.findUser();
 		verify(userDao);
+	}
+	
+	@Test
+	public void testAdd1(){
+		reset(userDao,roleDao,groupDao);
+		Integer[] rids = {1,2};
+		Integer[] gids = {2,3};
+		Role r = new Role(1,"管理员",RoleType.ROLE_ADMIN);
+		Group g = new Group(2,"财务处");
+		
+		expect(userDao.loadByUsername("admin1")).andReturn(null);
+		expect(userDao.add(baseUser)).andReturn(baseUser);
+		expect(roleDao.load(rids[0])).andReturn(r);
+		userDao.addUserRole(baseUser,r);
+		expectLastCall();
+		r.setId(2);
+		expect(roleDao.load(rids[1])).andReturn(r);
+		userDao.addUserRole(baseUser,r);
+		expectLastCall();
+		
+		expect(groupDao.load(gids[0])).andReturn(g);
+		userDao.addUserGroup(baseUser,g);
+		expectLastCall();
+		g.setId(3);
+		expect(groupDao.load(gids[1])).andReturn(g);
+		userDao.addUserGroup(baseUser,g);
+		expectLastCall();
+		
+		replay(userDao,roleDao,groupDao);
+		userService.add(baseUser,rids,gids);
+		verify(userDao,roleDao,groupDao);
+	}
+	
+	@Test
+	public void testAdd2(){
+		reset(userDao,roleDao,groupDao);
+		Integer[] rids = {1,2,5,6};
+		Integer[] gids = {2,3,4};
+		Role r = new Role(1,"管理员",RoleType.ROLE_ADMIN);
+		Group g = new Group(2,"财务处");
+		expect(userDao.loadByUsername("admin1")).andReturn(null);
+		//添加role
+		expect(userDao.add(baseUser)).andReturn(baseUser);
+		//动态参数
+		expect(roleDao.load(EasyMock.gt(0))).andReturn(r).times(4);
+		userDao.addUserRole(baseUser,r);
+		expectLastCall().times(4);
+		//添加group
+		expect(groupDao.load(EasyMock.gt(0))).andReturn(g).times(3);
+		userDao.addUserGroup(baseUser,g);
+		expectLastCall().times(3);
+		
+		replay(userDao,roleDao,groupDao);
+		userService.add(baseUser,rids,gids);
+		verify(userDao,roleDao,groupDao);
+	}
+	
+	@Test(expected=CmsException.class)
+	public void testAddNoRole(){
+		reset(userDao,roleDao,groupDao);
+		Integer[] rids = {1,2,5,6};
+		Integer[] gids = {2,3,4};
+		Role r = new Role(1,"管理员",RoleType.ROLE_ADMIN);
+		Group g = new Group(2,"财务处");
+		expect(userDao.loadByUsername("admin1")).andReturn(null);
+		//添加role
+		expect(userDao.add(baseUser)).andReturn(baseUser);
+		//动态参数
+		expect(roleDao.load(EasyMock.gt(0))).andReturn(null).times(4);
+		userDao.addUserRole(baseUser,r);
+		expectLastCall().times(4);
+		//添加group
+		expect(groupDao.load(EasyMock.gt(0))).andReturn(g).times(3);
+		userDao.addUserGroup(baseUser,g);
+		expectLastCall().times(3);
+		
+		replay(userDao,roleDao,groupDao);
+		userService.add(baseUser,rids,gids);
+		verify(userDao,roleDao,groupDao);
+	}
+	
+	@Test(expected=CmsException.class)
+	public void testAddNoGroup(){
+		reset(userDao,roleDao,groupDao);
+		Integer[] rids = {1,2,5,6};
+		Integer[] gids = {2,3,4};
+		Role r = new Role(1,"管理员",RoleType.ROLE_ADMIN);
+		Group g = new Group(2,"财务处");
+		expect(userDao.loadByUsername("admin1")).andReturn(null);
+		//添加role
+		expect(userDao.add(baseUser)).andReturn(baseUser);
+		//动态参数
+		expect(roleDao.load(EasyMock.gt(0))).andReturn(r).times(4);
+		userDao.addUserRole(baseUser,r);
+		expectLastCall().times(4);
+		//添加group
+		expect(groupDao.load(EasyMock.gt(0))).andReturn(null).times(3);
+		userDao.addUserGroup(baseUser,g);
+		expectLastCall().times(3);
+		
+		replay(userDao,roleDao,groupDao);
+		userService.add(baseUser,rids,gids);
+		verify(userDao,roleDao,groupDao);
+	}
+	
+	@Test(expected=CmsException.class)
+	public void testAddHasUser(){
+		reset(userDao,roleDao,groupDao);
+		Integer[] rids = {1,2,5,6};
+		Integer[] gids = {2,3,4};
+		expect(userDao.loadByUsername("admin1")).andReturn(baseUser);
+		
+		replay(userDao,roleDao,groupDao);
+		userService.add(baseUser,rids,gids);
+		verify(userDao,roleDao,groupDao);
+	}
+	
+	public void testUpdateUser(){
+		reset(userDao,roleDao,groupDao);
+		Integer[] nids = {1,2};
+		List<Integer> eids = Arrays.asList(2,3);
+		Role r = new Role();
+		Group g = new Group();
+		
+		//验证获取存在的角色id和组id
+		expect(userDao.listUserRoleIds(baseUser.getId())).andReturn(eids);
+		expect(userDao.listUserGroupIds(baseUser.getId())).andReturn(eids);
+		expect(roleDao.load(1)).andReturn(r);
+		//验证添加角色和组是否正确
+		userDao.addUserRole(baseUser,r);
+		expectLastCall();
+		expect(groupDao.load(1)).andReturn(g);
+		userDao.addUserGroup(baseUser,g);
+		expectLastCall();
+		//验证删除角色和组是否正确
+		userDao.deleteUserRole(baseUser.getId(),3);
+		userDao.deleteUserGroup(baseUser.getId(),3);
+		
+		replay(userDao,roleDao,groupDao);
+		userService.update(baseUser,nids,nids);
+		verify(userDao,roleDao,groupDao);
 	}
 }
